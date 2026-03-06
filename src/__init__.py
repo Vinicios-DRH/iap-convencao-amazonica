b654e69d78dbd3b918b1b43e8e227cd09afc60c7
Vinicios-Developer <viniciosgabriel_18@hotmail.com>
2026-03-06 11:38:54 -0400

    meta tags

--- src/__init__.py
+++ src/__init__.py
@@ -18,8 +18,8 @@ app = Flask(__name__)
 app.jinja_env.globals['get_b2_file_url'] = get_b2_file_url
 
 # Carrega as configurações do .env
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 app.jinja_env.globals.update(enumerate=enumerate)
 
